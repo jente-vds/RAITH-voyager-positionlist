62443c1cc25e323c55c5458843023b13e8e70f29
jente-vds <jente.vds@gmail.com>
2023-05-25 11:01:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
         name='voyager',
         version='0.9.3',
         author='Jente Vandersmissen',
-        author_email='j.vandersmissen@amolf.nl',
+        author_email='jente.vds@murena.io',
         license='LICENSE.txt',
         description='A package for the generation of positionlists and working area files for the RAITH VOYAGER.',
-        install_requires=["numpy", "matplotlib", "pandas"])
+        install_requires=["numpy", "matplotlib", "pandas"]
+)
